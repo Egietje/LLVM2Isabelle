d0c548401a373612e490bd8f8669fd3929050dc7
Thomas Kas <t.s.kas@student.utwente.nl>
2025-11-24 20:21:22 +0100

    Better structure

---
+++ Result.thy
@@ -0,0 +1,23 @@
+theory Result
+  imports "HOL-Library.Monad_Syntax"
+begin
+
+
+datatype error = unknown_register | uninitialized_register | register_override
+  | unallocated_address | uninitialized_address
+  | not_an_address | incompatible_types | unknown_label
+
+datatype 'a result = ok 'a | err error
+
+
+definition bind :: "'a result \<Rightarrow> ('a \<Rightarrow> 'b result) \<Rightarrow> 'b result" where
+  "bind R f = (case R of err e \<Rightarrow> err e | ok x \<Rightarrow> f x)"
+
+definition return :: "'a \<Rightarrow> 'a result" where
+  "return x = ok x"
+
+adhoc_overloading
+  Monad_Syntax.bind==bind
+
+
+end
