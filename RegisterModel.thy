764eebbe6c34dc87694aaaf0f2992708173ee2eb
Thomas Kas <t.s.kas@student.utwente.nl>
2025-12-11 20:45:35 +0100

    More wp lemmas

--- RegisterModel.thy
+++ RegisterModel.thy
@@ -30,12 +30,12 @@ lemma register_empty_get_unknown: "get_register empty_register_model n = err unk
   by simp
 
 
-lemma register_set_ok_unknown: "set_register r n v = ok r' \<Longrightarrow> get_register r n = err unknown_register"
+lemma register_set_ok_unknown: "get_register r n = err unknown_register \<longleftrightarrow> (\<exists>r'. set_register r n v = ok r')"
   unfolding set_register_def get_register_def
-  by (simp split: if_splits add: option.case_eq_if)
+  by (auto split: if_splits simp add: option.case_eq_if)
 
 
-lemma register_set_independent_get: "set_register r n' v' = ok r' \<Longrightarrow> n \<noteq> n' \<Longrightarrow> get_register r n = get_register r' n"
+lemma register_set_independent_get: "n \<noteq> n' \<Longrightarrow> set_register r n' v' = ok r' \<Longrightarrow> get_register r n = get_register r' n"
   unfolding get_register_def set_register_def
   by (cases "Mapping.lookup r n'"; auto)
 
@@ -53,12 +53,21 @@ lemma register_set_get_wp: "wp_ok (do { r' \<leftarrow> set_register r n v; get_
   by (simp add: option.case_eq_if)
 
 
-lemma register_get_ok_set: "get_register r n = ok v \<Longrightarrow> set_register r n v' = err register_override"
+lemma register_get_override: "get_register r n = ok v \<Longrightarrow> set_register r n v' = err register_override"
   unfolding set_register_def get_register_def option.case_eq_if
   by (simp split: if_splits)
 
-lemma register_set_set: "set_register r n v = ok r' \<Longrightarrow> set_register r' n v' = err register_override"
+lemma register_get_override_wp: "wp_ok (get_register r n) (\<lambda>_. set_register r n v = err register_override)"
+  unfolding get_register_def set_register_def
+  by auto
+
+
+lemma register_set_override: "set_register r n v = ok r' \<Longrightarrow> set_register r' n v' = err register_override"
   unfolding set_register_def option.case_eq_if
   by (auto split: if_splits)
+
+lemma register_set_override_wp: "wp_ok (set_register r n v) (\<lambda>r'. set_register r' n v' = err register_override)"
+  unfolding get_register_def set_register_def
+  by auto
 
 end
